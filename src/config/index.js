c9867432acef48f63b4ab6ce867f07a8651b4f29
chenxiaoqun <chenxqxs@163.com>
2021-01-17 09:59:32 +0800

    新增代理设置

--- src/config/index.js
+++ src/config/index.js
@@ -1,12 +1,21 @@
-let baseUrl = "";
+let configUrl = {};
 const env = process.env;
 
 if (env.NODE_ENV == "development") {
-  baseUrl = `http://localhost:3000`; //本地
+  configUrl = {
+    baseUrl: `/testApi/test`, //本地
+    web: `/testApi/test`, //网站
+  };
 } else if (env.NODE_ENV == "production") {
-  baseUrl = `http://192.168.30.47`; //生产环境地址93
+  configUrl = {
+    baseUrl: `http://192.168.30.47`, //本地
+    web: `http://192.168.30.47`, //网站
+  };
 } else if (env.NODE_ENV == "test") {
-  baseUrl = ``; //测试环境地址
+  configUrl = {
+    baseUrl: `http://192.168.30.47`, //本地
+    web: `http://192.168.30.47`, //网站
+  };
 }
 
-export { baseUrl, env };
+export { configUrl, env };
--- vue.config.js
+++ vue.config.js
@@ -35,52 +35,85 @@ const Timestamp = new Date().getTime();
 const path = require("path");
 const FileManagerPlugin = require("filemanager-webpack-plugin");
 
-const resolve = dir => {
-	return path.join(__dirname, dir);
+const resolve = (dir) => {
+  return path.join(__dirname, dir);
 };
 let filename_sub = "djplaza-" + process.env.NODE_ENV;
 let filename = filename_sub + "-" + Timestamp;
 module.exports = {
-	publicPath: "./",
-	assetsDir: "./static",
-	outputDir: filename,
-	productionSourceMap: false,
-	lintOnSave: true,
-	css: {
-		loaderOptions: {
-			postcss: {
-				// 这里的选项会传递给 postcss-loader
-				plugins: [
-					autoprefixer(),
-					pxtorem({
-						rootValue: 37.5, // 如果开发过程需要直接使用750设计稿尺寸，此处写75
-						propList: ["*"],
-						selectorBlackList: ["van-"]
-					})
-				]
-			}
-		}
-	},
-	configureWebpack: {
-		output: {
-			filename: `[name].${process.env.NODE_ENV}.${Timestamp}.js`,
-			chunkFilename: `[name].${process.env.NODE_ENV}.${Timestamp}.js`
-		}
-	},
-	chainWebpack: config => {
-		config.resolve.alias.set("@style", resolve("src/assets/style"));
-		config.resolve.alias.set("@components", resolve("src/components"));
-		config.resolve.alias.set("@img", resolve("src/assets/img"));
-		if (process.env.NODE_ENV !== "dev") {
-			config.plugin("zip").use(FileManagerPlugin, [{
-				onEnd: {
-					delete: ["*.zip"],
-					archive: [{
-						source: "./" + filename,
-						destination: "./" + filename + ".zip"
-					}]
-				}
-			}]);
-		}
-	}
+  publicPath: "./",
+  assetsDir: "./static",
+  outputDir: filename,
+  productionSourceMap: false,
+  lintOnSave: true,
+  css: {
+    loaderOptions: {
+      postcss: {
+        // 这里的选项会传递给 postcss-loader
+        plugins: [
+          autoprefixer(),
+          pxtorem({
+            rootValue: 37.5, // 如果开发过程需要直接使用750设计稿尺寸，此处写75
+            propList: ["*"],
+            selectorBlackList: ["van-"],
+          }),
+        ],
+      },
+    },
+  },
+  devServer: {
+    host: "localhost", //target host
+    port: 8080,
+    proxy: {
+      "/testApi/test": {
+        target: "http://www.eeee.net/",
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          "^/testApi/test": "/",
+        },
+      },
+      "/api": {
+        target: "http://www.eeee.com/",
+        changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
+        pathRewrite: {
+          "^/api": "/",
+        },
+      },
+    },
+  },
+  configureWebpack: {
+    output: {
+      filename: `[name].${process.env.NODE_ENV}.${Timestamp}.js`,
+      chunkFilename: `[name].${process.env.NODE_ENV}.${Timestamp}.js`,
+    },
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias.set("@style", resolve("src/assets/style"));
+    config.resolve.alias.set("@components", resolve("src/components"));
+    config.resolve.alias.set("@img", resolve("src/assets/img"));
+    // 删除zip包
+    if (process.env.NODE_ENV !== "dev") {
+      config.plugin("zip").use(FileManagerPlugin, [
+        {
+          onEnd: {
+            delete: ["*.zip"],
+            archive: [
+              {
+                source: "./" + filename,
+                destination: "./" + filename + ".zip",
+              },
+            ],
+          },
+        },
+      ]);
+    }
+    // 删除打包文件包
+    config.plugin().use(FileManagerPlugin, [
+      {
+        onStart: {
+          delete: ["./" + filename_sub + "*"],
+        },
+      },
+    ]);
+  },
 };
